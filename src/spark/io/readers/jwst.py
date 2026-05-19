0bc6083594757d9cb2b037f7f43ffd3bd2eabdd5
galactic-fireball <sdoan1@gmail.com>
2026-05-19 09:24:02 -0400

    add wcs and pix_scale to jwst

--- src/spark/io/readers/jwst.py
+++ src/spark/io/readers/jwst.py
@@ -1,15 +1,18 @@
 import astropy.units as u
+from astropy.wcs import WCS
 from dataclasses import dataclass
 import numpy as np
 import pathlib
 
-from spark.io.models import SparkCube
+from spark.io.models import SparkInstrument, SparkCube, ObservationTarget
 import spark.constants as sc
 
 JWST_WAVE_UNIT = u.um
 
 @dataclass
 class JWSTCube(SparkCube):
+
+	pix_scale: u.Unit = 0.0
 
 	@classmethod
 	def from_fits(cls, hdu, **kwargs):
@@ -27,6 +30,7 @@ class JWSTCube(SparkCube):
 		wave0 = header['CRVAL3'] - (header['CRPIX3'] - 1) * header['CDELT3']
 		obs_wave = (wave0 + np.arange(nwave)*header['CDELT3']) * cunit
 
+		# transpose to: (nx, ny, nwave)
 		cube_spec = hdu['SCI'].data.T * bunit
 		cube_err = hdu['ERR'].data.T * bunit
 		if ('/sr' in str(bunit)) or ('/ sr' in str(bunit)):
@@ -39,15 +43,23 @@ class JWSTCube(SparkCube):
 		cube_err[np.isnan(cube_err)] = np.nanmedian(cube_err)
 
 		inst_data = {}
+		inst_data['wcs'] = WCS(hdu['SCI'])
 		inst_data['wave'] = obs_wave
 		inst_data['wave_unit'] = cunit
 		inst_data['flux'] = cube_spec
 		inst_data['err'] = cube_err
 		inst_data['flux_unit'] = sc.FLUX_UNIT_UM
-		print('WARNING: JWST fits headers do not store redshift data, please supply your own if needed')
-		inst_data['z'] = 0.0
-		inst_data['ra'] = hdu[0].header['TARG_RA']
-		inst_data['dec'] = hdu[0].header['TARG_DEC']
+		# print('WARNING: JWST fits headers do not store redshift data, please supply your own if needed')
+
+		inst_data['target'] = ObservationTarget(
+			name = hdu[0].header['TARGNAME'],
+			ra = hdu[0].header['TARG_RA'],
+			dec = hdu[0].header['TARG_DEC'],
+			z = kwargs.pop('z', 0.0),
+			instrument = SparkInstrument(name = hdu[0].header['INSTRUME']),
+		)
+
+		inst_data['pix_scale'] = (header['CDELT1'] * u.Unit(header['CUNIT1'])).to(u.arcsec)
 
 		inst_data.update(kwargs)
 		if hdu_close: hdu.close()
