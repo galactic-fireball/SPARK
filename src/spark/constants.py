7ce065122836b87ad38e763590d2fc71d71baf94
galactic-fireball <sdoan1@gmail.com>
2026-05-19 09:22:18 -0400

    constant labels

--- src/spark/constants.py
+++ src/spark/constants.py
@@ -6,3 +6,10 @@ WAVE_UNIT_UM = u.um
 INT_FLUX = u.erg / u.s / (u.cm**2)
 FLUX_UNIT_UM = u.erg / u.s / (u.cm**2) / u.um
 FLUX_UNIT_AA = u.erg / u.s / (u.cm**2) / u.AA
+
+
+FLUX_AA_LABEL = r'$f_\lambda (~\rm{erg}~\rm{cm}^{-2}~\rm{s}^{-1}\rm{\AA}^{-1})$'
+WAVE_AA_LABEL = r'$\lambda_{rest} (\AA)$'
+FLUX_UM_LABEL = r'$f_\lambda (~\rm{erg}~\rm{cm}^{-2}~\rm{s}^{-1}\rm{\mu m}^{-1})$'
+WAVE_UM_LABEL = r'$\lambda_{rest} (\mu m)$'
+INT_FLUX_LABEL = r'$F (~\rm{erg}~\rm{cm}^{-2}~\rm{s}^{-1})$'
